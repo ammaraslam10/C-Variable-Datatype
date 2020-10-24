c98496cb3717e229409b68266298800f0d6b30b7
ammaraslam10 <40186412+ammaraslam10@users.noreply.github.com>
2020-10-24 07:50:48 +0500

    Update var.hpp

--- var.hpp
+++ var.hpp
@@ -46,8 +46,8 @@ class var {
 	}
 public:
 	var() { dtype = 'n'; } 
-	var(const int data) { i = data; dtype = 'i'; } 
-	var(const double data) { d = data; dtype = 'd'; } 
+	var(const int data, bool fix = 0) { i = data; dtype = 'i'; }
+	var(const double data, bool fix = 0) { d = data; dtype = 'd'; }
 	var(const std::string data) { s = data; dtype = 's'; }
 	var(const char* data) { s = data; dtype = 's'; } 
 	var(const var& a) { 
@@ -55,10 +55,18 @@ public:
 			return;
 		d = a.d; i = a.i; s = a.s; dtype = a.dtype;
 	}
+	var(const std::string data, bool fix) { 
+		s = data; 
+		dtype = whatType(s); 
+		if (dtype == 'i') i = toInt(s);
+		else if (dtype == 'd') d = toDbl(s);
+	}
+	var(const char* data, bool fix):var((std::string) data, 1) { }
 
 	var& operator=(const int data) { i = data; dtype = 'i'; return *this; }
 	var& operator=(const double data) { d = data; dtype = 'd'; return *this; }
-	//var& operator=(char data) { s = data; dtype = ''; return *this; }
+	var& operator=(char data) { s = data; dtype = 's'; return *this; }
+	var& operator=(char* data) { s = data; dtype = 's'; return *this; }
 	var& operator=(const std::string data) { s = data; dtype = 's'; return *this; }
 	var& operator=(const var& data) {
 		if (this == &data)
@@ -67,29 +75,36 @@ public:
 		return *this;
 	}
 
-	bool operator==(const var& data) const {
-		if (dtype == 'i' && data.dtype == 'i')
-			return i == data.i;
-		else if (dtype == 'd' && data.dtype == 'd')
-			return d == data.d;
-		else if (dtype == 's' && data.dtype == 's')
-			return s == data.s;
-		else if (dtype == 'i' && data.dtype == 'd')
-			return i == data.d;
-		else if (dtype == 'd' && data.dtype == 'i')
-			return d == data.i;
-		else if (dtype == 'i' && data.dtype == 's')
-			return convert(i) == data.s;
-		else if (dtype == 'd' && data.dtype == 's')
-			return convert(d) == data.s;
-		else if (data.dtype == 'i' && dtype == 's')
-			return convert(data.i) == s;
-		else if (data.dtype == 'd' && dtype == 's')
-			return convert(data.d) == s;
-		else if (data.dtype == 'n' && dtype == 'n')
-			return true;
-		return false;
+	#define OPRFUNC(X) bool operator X (const var& data) const { \
+		if (dtype == 'i' && data.dtype == 'i') \
+			return i X data.i; \
+		else if (dtype == 'd' && data.dtype == 'd') \
+			return d X data.d; \
+		else if (dtype == 's' && data.dtype == 's') \
+			return s X data.s; \
+		else if (dtype == 'i' && data.dtype == 'd') \
+			return i X data.d; \
+		else if (dtype == 'd' && data.dtype == 'i') \
+			return d X data.i; \
+		else if (dtype == 'i' && data.dtype == 's') \
+			return convert(i) X data.s; \
+		else if (dtype == 'd' && data.dtype == 's') \
+			return convert(d) X data.s; \
+		else if (data.dtype == 'i' && dtype == 's') \
+			return convert(data.i) X s; \
+		else if (data.dtype == 'd' && dtype == 's') \
+			return convert(data.d) X s; \
+		else if (data.dtype == 'n' && dtype == 'n') \
+			return true; \
+		return false; \
 	}
+	OPRFUNC(==);
+	OPRFUNC(>);
+	OPRFUNC(<);
+	OPRFUNC(>=);
+	OPRFUNC(<=);
+	OPRFUNC(!=);
+	#undef OPRFUNC
 	bool equals(const var& data) const {
 		if (dtype != data.dtype)
 			return false;
@@ -102,62 +117,6 @@ public:
 		else if (dtype == 'n')
 			return true;
 		return false;
-	}
-	bool operator>(const var& data) const {
-		if (dtype == 'i' && data.dtype == 'i')
-			return i > data.i;
-		else if (dtype == 'd' && data.dtype == 'd')
-			return d > data.d;
-		else if (dtype == 's' && data.dtype == 's')
-			return s > data.s;
-		else if (dtype == 'i' && data.dtype == 'd')
-			return i > data.d;
-		else if (dtype == 'd' && data.dtype == 'i')
-			return d > data.i;
-		else if (dtype == 'i' && data.dtype == 's')
-			return convert(i) > data.s;
-		else if (dtype == 'd' && data.dtype == 's')
-			return convert(d) > data.s;
-		else if (data.dtype == 'i' && dtype == 's')
-			return convert(data.i) > s;
-		else if (data.dtype == 'd' && dtype == 's')
-			return convert(data.d) > s;
-		else if (data.dtype == 'n' && dtype == 'n')
-			return true;
-		return false;
-	}
-	bool operator<(const var& data) const {
-		if (dtype == 'i' && data.dtype == 'i')
-			return i < data.i;
-		else if (dtype == 'd' && data.dtype == 'd')
-			return d < data.d;
-		else if (dtype == 's' && data.dtype == 's')
-			return s < data.s;
-		else if (dtype == 'i' && data.dtype == 'd')
-			return i < data.d;
-		else if (dtype == 'd' && data.dtype == 'i')
-			return d < data.i;
-		else if (dtype == 'i' && data.dtype == 's')
-			return convert(i) < data.s;
-		else if (dtype == 'd' && data.dtype == 's')
-			return convert(d) < data.s;
-		else if (data.dtype == 'i' && dtype == 's')
-			return convert(data.i) < s;
-		else if (data.dtype == 'd' && dtype == 's')
-			return convert(data.d) < s;
-		else if (data.dtype == 'n' && dtype == 'n')
-			return true;
-		return false;
-
-	}
-	bool operator<=(const var& data) const {
-		return (*this) < data || (*this) == data;
-	}
-	bool operator>=(const var& data) const {
-		return (*this) > data || (*this) == data;
-	}
-	bool operator!=(const var& data) const {
-		return !((*this) == data);
 	}
 	// +
 	var operator+(const var& data) const {
@@ -181,41 +140,113 @@ public:
 			return convert(data.d) + s;
 		return var();
 	}
-	// -
-	var operator-(const var& data) const {
+	// +=
+	var operator+=(const var& data) {
 		if (dtype == 'i' && data.dtype == 'i')
-			return i - data.i;
+			i += data.i;
 		else if (dtype == 'd' && data.dtype == 'd')
-			return d - data.d;
-		else if (dtype == 'i' && data.dtype == 'd')
-			return i - data.d;
-		else if (dtype == 'd' && data.dtype == 'i')
-			return d - data.i;
+			d += data.d;
+		else if (dtype == 's' && data.dtype == 's')
+			s += data.s;
+		else if (dtype == 'i' && data.dtype == 'd') {
+			d = i + data.d;
+			dtype = 'd';
+		} else if (dtype == 'd' && data.dtype == 'i')
+			d += data.i;
+		else if (dtype == 'i' && data.dtype == 's') {
+			s = convert(i) + data.s;
+			dtype = 's';
+		} else if (dtype == 'd' && data.dtype == 's') {
+			s = convert(d) + data.s;
+			dtype = 's';
+		} else if (data.dtype == 'i' && dtype == 's') {
+			s = convert(data.i) + s;
+			dtype = 's';
+		} else if (data.dtype == 'd' && dtype == 's') {
+			s = convert(data.d) + s;
+			dtype = 's';
+		}
 		return var();
 	}
-	// *
-	var operator*(const var& data) const {
-		if (dtype == 'i' && data.dtype == 'i')
-			return i * data.i;
-		else if (dtype == 'd' && data.dtype == 'd')
-			return d * data.d;
-		else if (dtype == 'i' && data.dtype == 'd')
-			return i * data.d;
-		else if (dtype == 'd' && data.dtype == 'i')
-			return d * data.i;
-		return var();
+	#define OPRFUNC(X) var operator X (const var& data) const { \
+		if (dtype == 'i' && data.dtype == 'i') \
+			return i X data.i; \
+		else if (dtype == 'd' && data.dtype == 'd') \
+			return d X data.d; \
+		else if (dtype == 'i' && data.dtype == 'd') \
+			return i X data.d; \
+		else if (dtype == 'd' && data.dtype == 'i') \
+			return d X data.i; \
+		return var(); \
 	}
-	// /
+	OPRFUNC(-);
+	OPRFUNC(*);
+	#undef OPRFUNC
 	var operator/(const var& data) const {
 		if (dtype == 'i' && data.dtype == 'i')
-			return i / data.i;
-		else if (dtype == 'd' && data.dtype == 'd')
+			return i / (double) data.i;
+		else if (dtype == 'd' && data.dtype == 'd') 
 			return d / data.d;
-		else if (dtype == 'i' && data.dtype == 'd')
+		else if (dtype == 'i' && data.dtype == 'd') 
 			return i / data.d;
-		else if (dtype == 'd' && data.dtype == 'i')
+		else if (dtype == 'd' && data.dtype == 'i') 
 			return d / data.i;
+		return (*this);
+	}
+	var operator%(const var& data) const {
+		if (dtype == 'i' && data.dtype == 'i')
+			return i % data.i;
+		else if (dtype == 'd' && data.dtype == 'd')
+			return (int)d % (int)data.d;
+		else if (dtype == 'i' && data.dtype == 'd')
+			return i % (int)data.d;
+		else if (dtype == 'd' && data.dtype == 'i')
+			return (int)d % data.i;
 		return var();
+	}
+	#define OPRFUNC(X) var& operator X (const var& data) { \
+		if (dtype == 'i' && data.dtype == 'i') \
+			i X data.i; \
+		else if (dtype == 'd' && data.dtype == 'd') \
+			d X data.d; \
+		else if (dtype == 'i' && data.dtype == 'd') { \
+			d = i; \
+			d X data.d; \
+			dtype = 'd'; \
+		} else if (dtype == 'd' && data.dtype == 'i') \
+			d X data.i; \
+		return (*this); \
+	}
+	OPRFUNC(-=);
+	OPRFUNC(*=);
+	#undef OPRFUNC
+	var& operator/=(const var& data) {
+		if (dtype == 'i' && data.dtype == 'i')
+			if(i % data.i == 0) i = data.i;
+			else { d = i / data.i; dtype = 'd'; }
+		else if (dtype == 'd' && data.dtype == 'd') {
+			d /= data.d;
+		} else if (dtype == 'i' && data.dtype == 'd') {
+			d = i / data.d;
+			dtype = 'd';
+		} else if (dtype == 'd' && data.dtype == 'i') {
+			d /= data.i;
+		}
+		return (*this);
+	}
+	var& operator%=(const var& data) {
+		if (dtype == 'i' && data.dtype == 'i')
+			i %= data.i;
+		else if (dtype == 'd' && data.dtype == 'd') {
+			i = (int)d % (int)data.d;
+			dtype = 'i';
+		} else if (dtype == 'i' && data.dtype == 'd')
+			i %= (int) data.d;
+		else if (dtype == 'd' && data.dtype == 'i') {
+			i = (int)d % data.i;
+			dtype = 'i';
+		}
+		return (*this);
 	}
 	char& operator[](const int a) {
 		if (dtype == 's')
@@ -257,7 +288,7 @@ public:
 		if (dtype == 'i')
 			return convert(i);
 		else if (dtype == 'd')
-			return convert(i);
+			return convert(d);
 		else if (dtype == 's')
 			return s;
 		else
@@ -277,6 +308,7 @@ public:
 			return toDbl(s);
 		return 0;
 	}
+	std::string parseString() const { return toString(); }
 	bool isNull() {
 		return dtype == 'n';
 	}
@@ -329,6 +361,21 @@ public:
 	~var() {
 		dtype = 'n';
 	}
+	#define FRIENDEXP(X, T) \
+	friend T operator X (const var, const int); friend T operator X (const int, const var); \
+	friend T operator X (const var, const double); friend T operator X (const double, const var); \
+	friend T operator X (const var, const char*); friend T operator X (const char*, const var); \
+	friend T operator X (const var, const std::string); friend T operator X (const std::string, const var); 
+	FRIENDEXP(== ,bool); FRIENDEXP(> ,bool); FRIENDEXP(< ,bool); FRIENDEXP(>= ,bool); FRIENDEXP(<= ,bool); FRIENDEXP(!= ,bool);
+	FRIENDEXP(+, var); FRIENDEXP(-, var); FRIENDEXP(/ , var); FRIENDEXP(*, var); FRIENDEXP(%, var);
+	#undef FRIENDEXP
+	#define FRIENDEXP(X) \
+	friend var& operator X (var&, const int); friend int& operator X (int&, const var); \
+	friend var& operator X (var&, const double); friend double& operator X (double&, const var); \
+	friend var& operator X (var&, const char*); \
+	friend var& operator X (var&, const std::string); friend std::string& operator X (std::string&, const var); 
+	FRIENDEXP(+=); FRIENDEXP(-=); FRIENDEXP(/=); FRIENDEXP(*=); FRIENDEXP(%=);
+	#undef FRIENDEXP
 };
 std::ostream& operator<<(std::ostream& s, const var& a) {
 	s << a.toString();
@@ -343,4 +390,40 @@ std::istream& operator>>(std::istream& s, var& a) {
 		a.convertDouble();
 	return s; 
 }
+#define OPRFUNC(X, T1, RET) RET operator X (const var data, const T1 val) { \
+	return data X var(val, 1); \
+}
+#define OPRFUNC2(X, T1, RET) RET operator X (const T1 val, const var data) { \
+	return var(val, 1) X data; \
+}
+#define OPRFUNCMAC(X, T) \
+OPRFUNC(X , int, T); OPRFUNC2(X , int, T); \
+OPRFUNC(X , double, T); OPRFUNC2(X , double, T); \
+OPRFUNC(X , char*, T); OPRFUNC2(X , char*, T); \
+OPRFUNC(X , std::string, T); OPRFUNC2(X , std::string, T);
+
+OPRFUNCMAC(== ,bool); OPRFUNCMAC(> ,bool); OPRFUNCMAC(< ,bool); OPRFUNCMAC(>= ,bool); OPRFUNCMAC(<= ,bool); OPRFUNCMAC(!= ,bool);
+OPRFUNCMAC(+, var); OPRFUNCMAC(-, var); OPRFUNCMAC(/ , var); OPRFUNCMAC(*, var); OPRFUNCMAC(% , var);
+#undef OPRFUNC
+#undef OPRFUNC2
+#undef OPRFUNCMAC
+#define OPRFUNCMAC(W, X, T, F) \
+T& operator W (T& val, const var data) { \
+	val = (var(val, 1) X data).parse##F(); \
+	return val; \
+} \
+var& operator W(var& data, const T val){ \
+	data = (data X var(val, 1)).parse##F(); \
+	return data; \
+}
+OPRFUNCMAC(+=, +, int, Int); OPRFUNCMAC(+=, +, double, Double); OPRFUNCMAC(+=, +, std::string, String);
+OPRFUNCMAC(-=, -, int, Int); OPRFUNCMAC(-=, -, double, Double); OPRFUNCMAC(-=, -, std::string, String);
+OPRFUNCMAC(*=, *, int, Int); OPRFUNCMAC(*=, *, double, Double); OPRFUNCMAC(*=, *, std::string, String);
+OPRFUNCMAC(/=, /, int, Int); OPRFUNCMAC(/=, /, double, Double); OPRFUNCMAC(/=, /, std::string, String);
+OPRFUNCMAC(%=, %, int, Int); OPRFUNCMAC(%=, %, double, Double); OPRFUNCMAC(%=, %, std::string, String);
+var& operator+=(var& data, const char* val) {
+	data = (data += var(val, 1)).parseString();
+	return data;
+}
+#undef OPRFUNCMAC
 #endif
